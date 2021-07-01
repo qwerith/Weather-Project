4d0de083e47a0d1a215216ea8eaeb1756e20a470
qwerith <yuriisorokin98@gmail.com>
2021-07-01 20:43:46 +0300

    Add Cache class Add input_type_check and string_handler

--- weather.py
+++ weather.py
@@ -4,54 +4,96 @@ import urllib3, requests, bs4, sys, os, json, datetime, re
 # from flask import Flask, url_for
 # from flask import render_template
 
-data_directory = r"C:\Users\Yura\Documents\weather project\data\ "
+data_path = r"C:\Users\Yura\Documents\weather project\data\ "
 API_KEY = os.getenv("WEATHER_KEY")
 if not API_KEY:
     raise RuntimeError("API key error")
 
 
-def input_handler():
-    location = input("Enter location name: ")
-    pattern = '.*(\d{4,}).*(\d{4,}).*'
+def input_type_check(location):
+    pattern = '.*(\d{2,}).*(\d{2,}).*'
+    if not re.search(pattern, location):
+        return "name"
+    else:
+        return "coords"
+
+
+def string_handler(location):
     pattern_replace = "[^. -1234567890]"
-    if len(location) > 30 or len(location) <= 2:
-        location = "Lviv"
-    elif not re.search(pattern, location):
+    if input_type_check(location) == "name":
         location = "q=" + location
-        print(location)
-    else: 
+    else:
         location = re.sub(pattern_replace,'',location)
-        print(location)
         location = (re.sub(r'''[,'"!@#$%^&*()_+=|/?>,<`~]''',"", location)).lstrip(" ").rstrip(" ").split(" ")
-        print(location)
         lat = "lat=" + str(location[0])
         lon = "&lon=" + str(location[-1])
         location = lat + lon
-        print(lat, "\n",lon)
+    return location
+
+
+def input_handler():
+    location = input("Enter location name: ")
+    if len(location) > 30 or len(location) <= 2:
+        location = "q=Lviv"
+    else:
+        location = string_handler(location)
+    return location
+
+
+def get_coords_or_name(location, request):
+    pattern = '.*(\d{2,}).*(\d{2,}).*'
+    if re.search(pattern, location):
+        location = request["city"]["name"]
+    else:
+        location = str(request["city"]["coord"])
+        location = string_handler(location)
     return location
 
 
 def get_weather(API_KEY):
     #try:
-    location = input_handler()
-    print(location)
+    location_input = input_handler()
+    print(location_input)
     #except: TypeError("Invalid input")
-    url = f"http://api.openweathermap.org/data/2.5/forecast?{location}&units=metric&appid={API_KEY}"
+    url = f"http://api.openweathermap.org/data/2.5/forecast?{location_input}&units=metric&appid={API_KEY}"
     request = requests.get(url)
     print(request.status_code)
     if request.status_code != 200:
         raise RuntimeError("Request failed", request.status_code)
     else:
-        file_name = location[2: ] + ".text"
-        f = open(data_directory + file_name, "w")
-        json.dump(request.json(), f)
-        f.close()
         request = request.json()
+        if input_type_check(location_input) == "coords":
+            location = get_coords_or_name(location_input, request)
+            coords = location_input
+        else:
+            coords = get_coords_or_name(location_input, request)
+            location = location_input[2: ]
+            print(coords)
+        cach = Cache(location, coords)
+        cach.write(request)
+        cach.read()
     return request
 
 
-resault = get_weather(API_KEY)
+class Cache():
 
-place_name = resault["city"]["name"]
-dev_file = json.load((open(data_directory + place_name  + ".text", "r")))
-print(json.dumps(dev_file, indent = 4, sort_keys=True))
+    def __init__(self, location_name, coords):
+        self.location_name = location_name
+        self.coords = coords
+    
+    def write(self, request_data):
+        location_name = self.location_name
+        coords = self.coords
+        file_name = location_name + " " + coords + ".text"
+        f = open(data_path.rstrip(" ") + file_name, "w")
+        json.dump(request_data, f)
+        f.close()
+    
+    def read(self):
+        location_name = self.location_name
+        coords = self.coords
+        dev_file = json.load((open(data_path.rstrip(" ") + location_name + " " + coords + ".text", "r")))
+        print(json.dumps(dev_file, indent = 4, sort_keys=True))
+
+
+resault = get_weather(API_KEY)
