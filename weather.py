67f7bf8c1a80a4abe85d4af11df3e7409ed82dca
qwerith <yuriisorokin98@gmail.com>
2021-07-02 20:16:28 +0300

    Add cache_check function Add minor changes

--- weather.py
+++ weather.py
@@ -1,10 +1,9 @@
-import urllib3, requests, bs4, sys, os, json, datetime, re
-#from bs4 import BeautifulSoup
+import requests, sys, os, json, datetime, re
 # import flask
 # from flask import Flask, url_for
 # from flask import render_template
 
-data_path = r"C:\Users\Yura\Documents\weather project\data\ "
+data_path = os.path.join("C:", os.sep, "Users", "Yura", "Documents", "weather project", "data","")
 API_KEY = os.getenv("WEATHER_KEY")
 if not API_KEY:
     raise RuntimeError("API key error")
@@ -31,7 +30,7 @@ def string_handler(location):
     return location
 
 
-def input_handler():
+def get_input():
     location = input("Enter location name: ")
     if len(location) > 30 or len(location) <= 2:
         location = "q=Lviv"
@@ -50,28 +49,43 @@ def get_coords_or_name(location, request):
     return location
 
 
+def cache_check(location):
+    if input_type_check(location) == "name":
+        for x in os.listdir(data_path):
+            if x.split(" ")[0] == location:
+                coords = re.sub(r'.text','', x.split(" ")[1])
+                cache = Cache(location, coords)
+                cache.read()
+                return True
+        return None 
+    else:
+        return print("TODO")
+
+
 def get_weather(API_KEY):
     #try:
-    location_input = input_handler()
+    location_input = get_input()
     print(location_input)
     #except: TypeError("Invalid input")
-    url = f"http://api.openweathermap.org/data/2.5/forecast?{location_input}&units=metric&appid={API_KEY}"
-    request = requests.get(url)
-    print(request.status_code)
-    if request.status_code != 200:
-        raise RuntimeError("Request failed", request.status_code)
-    else:
-        request = request.json()
-        if input_type_check(location_input) == "coords":
-            location = get_coords_or_name(location_input, request)
-            coords = location_input
+    if cache_check(location_input[2: ]) == None:
+        request = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?{location_input}&units=metric&appid={API_KEY}")
+        print(request.status_code)
+        if request.status_code != 200:
+            raise RuntimeError("Request failed", request.status_code)
         else:
-            coords = get_coords_or_name(location_input, request)
-            location = location_input[2: ]
-            print(coords)
-        cach = Cache(location, coords)
-        cach.write(request)
-        cach.read()
+            request = request.json()
+            if input_type_check(location_input) == "coords":
+                location = get_coords_or_name(location_input, request)
+                coords = location_input
+            else:
+                coords = get_coords_or_name(location_input, request)
+                location = location_input[2: ]
+                print(coords)
+            cache = Cache(location, coords)
+            cache.write(request)
+            cache.read()
+    else:
+        request = None
     return request
 
 
@@ -85,15 +99,16 @@ class Cache():
         location_name = self.location_name
         coords = self.coords
         file_name = location_name + " " + coords + ".text"
-        f = open(data_path.rstrip(" ") + file_name, "w")
+        f = open(data_path + file_name, "w")
         json.dump(request_data, f)
         f.close()
     
     def read(self):
         location_name = self.location_name
         coords = self.coords
-        dev_file = json.load((open(data_path.rstrip(" ") + location_name + " " + coords + ".text", "r")))
+        dev_file = json.load((open(data_path + location_name + " " + coords + ".text", "r")))
         print(json.dumps(dev_file, indent = 4, sort_keys=True))
 
 
 resault = get_weather(API_KEY)
+print(resault)
