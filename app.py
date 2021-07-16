07feea47bbc37f3236d08554cdf307376618d9f2
qwerith <yuriisorokin98@gmail.com>
2021-07-16 20:00:33 +0300

    Add bootstrap, additional data parse in app.py Changed index.html, styles.css

--- app.py
+++ app.py
@@ -7,12 +7,24 @@ app = Flask(__name__)
 def index():
     if request.method == "POST" and request.form.get("location"):
         print(request.form.get("location"))
-        DATA = get_weather(request.form.get("location"))
-        print(DATA)
-        try:
-            return render_template("index.html", data=DATA)
-        except: render_template("index.html")
-         
+        location = request.form.get("location")
+        DATA = get_weather(location)
+        data_list = []
+        group_list = []
+        STATUS = f"{location} not found"
+        for i in DATA.keys():
+            group_date = i
+            break
+        for i in DATA.items():
+            i_date = i[0]
+            if str(group_date).split(" ")[0] == str(i_date).split(" ")[0]:
+                group_list.append(i)
+            else:
+                data_list.append(group_list)
+                group_list = []
+                group_date = str(i[0])   
+        print(data_list)
+        return render_template('index.html', status=STATUS) if type(DATA) == RuntimeError else render_template("index.html", data=data_list) 
     else:
         return render_template("index.html")
 
