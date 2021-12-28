eb00844f5978c5d82063f50902d5145a9b3efbb6
qwerith <yuriisorokin98@gmail.com>
2021-12-28 23:12:24 +0200

    Add fix to cron job and template

--- cron.py
+++ cron.py
@@ -1,13 +1,15 @@
 from weather import get_weather
 from mailing import query_mailing_table, compose_weather_mail_msg, send_gmail
+from app import convert_timestamp
 
 # Called by cron job every morning in 6:00 to send emails with weather data
 def cron_mailing():
     mailing_list = query_mailing_table()
     if type(mailing_list) != RuntimeError:
         for i in mailing_list:
-            data = compose_weather_mail_msg(get_weather(i[0]))
-            if not send_gmail(data, i[1]):
+            DATA = get_weather(i[0])
+            DATA = convert_timestamp(DATA, DATA[0][0][1]['timezone'])
+            if not send_gmail(compose_weather_mail_msg(DATA), i[1]):
                 #print("succsess")
                 return True
     else:
